942e7313f54941e43aee7279bffb24191a704f53
Taha shamji <tahashamji45@gmail.com>
2024-10-19 17:09:13 +0500

    Project Initialize

---
+++ src/test/java/com/project/ContactManagementSystem/ContactManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ContactManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
